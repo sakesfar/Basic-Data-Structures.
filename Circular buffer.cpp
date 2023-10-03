b45804c0f5107b4ccf94046398197ec11b0af2c5
sakesfar <121855106+sakesfar@users.noreply.github.com>
2023-10-03 16:35:27 +0600

    Circular buffer.cpp

--- Circular buffer.cpp
+++ Circular buffer.cpp
@@ -1,112 +1,62 @@
-#include <iostream>
-#include <cassert>
-
 template <typename Object>
-
-class RingBuffer
-{
+class RingBuffer {
 private:
+    int m_capacity;
     int m_size;
     Object* m_data;
     int m_head;
     int m_tail;
+
 public:
-    RingBuffer(int size = 0) : m_size{ size }
-    {
-        if (m_size == 0)
+    RingBuffer(int capacity = 0) : m_capacity{ capacity }, m_size{0}, m_head{0}, m_tail{0} {
+        if (m_capacity == 0) {
             m_data = nullptr;
-        else
-        {
-            m_data = new Object[m_size];
-            m_head = -1;
-            m_tail = -1;
+        } else {
+            m_data = new Object[m_capacity];
         }
     }
 
     ~RingBuffer() { delete[] m_data; }
 
-    void insert(const Object& obj)
-    {
+    bool isFull() const {
+        return m_size == m_capacity;
+    }
+
+    bool isEmpty() const {
+        return m_size == 0;
+    }
+
+    void insert(const Object& obj) {
         assert(m_data != nullptr);
 
-        if (m_tail == -1 && m_head == -1)
-        {
-            ++m_tail;
-            ++m_head;
-            m_data[m_tail] = obj;
+        if (isFull()) {
+            // Decide your behavior here: reject or overwrite.
+            // For this example, we'll overwrite the oldest data.
+            m_head = (m_head + 1) % m_capacity;
         }
 
-        else if (m_tail >= 0 && m_tail < m_size - 1)
-        {
-            ++m_tail;
-            m_data[m_tail] = obj;
-            
-            
-        }
-
-        else if (m_tail >= m_size - 1)
-        {
-            m_tail = 0;
-            m_data[m_tail] = obj;
-            ++m_tail;
-        }
-
-
+        m_data[m_tail] = obj;
+        m_tail = (m_tail + 1) % m_capacity;
+        m_size++;
     }
 
-    void pop()
-    {
-        assert(m_head != -1 || m_tail != -1);
+    void pop() {
+        assert(!isEmpty());
 
-        if (m_head >= 0 && m_head<m_tail)
-        {
-            m_data[m_head] = 0;
-            ++m_head;
-        }
-
-        if (m_head == m_tail)
-        {
-            m_data[m_head] = 0;
-            m_head = m_tail = -1;
-        }
-
+        m_head = (m_head + 1) % m_capacity;
+        m_size--;
     }
 
-    void printData()
-    {
-        assert(m_head != -1 && m_tail != -1 || m_data!=nullptr);
-        for (int i = 0; i < m_size; ++i)
-        {
-            std::cout << m_data[i] << '\n';
+    void printData() {
+        assert(m_data != nullptr);
+
+        for (int i = 0; i < m_capacity; ++i) {
+            std::cout << m_data[i] << ' ';
         }
-        std::cout << "HEAD:" << m_data[m_head] << ", TAIL:" << m_data[m_tail] << '\n';
+        std::cout << "\nHEAD:" << m_data[m_head] << ", TAIL:" << (m_tail == 0 ? m_data[m_capacity - 1] : m_data[m_tail - 1]) << '\n';
     }
 
-    int size()
-    {
+    int size() const {
         return m_size;
     }
-
 };
-
-int main()
-{
-    RingBuffer<int> list{8};
-    for (int i = 0; i < list.size(); ++i)
-    {
-        list.insert(i);
-    }
-
-    for (int i = 0; i < list.size() - 4; ++i)
-    {
-        list.pop();
-    }
-    list.printData();
-    list.insert(8);
-    std::cout << "INSERTION AFTER POPPING\n";
-    list.printData();
-
-
-    return 0;
-}
-
